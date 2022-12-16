45ca538fc70c27c4d3ec170aef07bd64485111ce
NikosVlachakis <nvlachakis1999@gmail.com>
2022-12-16 21:00:16 +0200

    finishing q3 with df

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import col, sum, desc, row_number, asc, max, month, dayofmonth, hour
+from pyspark.sql.functions import col, sum, desc, row_number, asc, max, month, dayofmonth, hour,round,floor
 import sys,time,os
+from pyspark.sql.window import Window
+from pyspark.sql.functions import col, row_number
 
 
 
@@ -26,32 +28,63 @@ rdd_taxi_zone_lookup = df_taxi_zone_lookup.rdd
 
 # Query 1
 
-start_Q1 = time.time()
+# start_Q1 = time.time()
 
 # Να βρεθεί η διαδρομή με το μεγαλύτερο φιλοδώρημα (tip)τον Μάρτιο και σημείο άφιξης το "BatteryPark"
-df_taxi_trips.filter(month(col("tpep_pickup_datetime")) == 3)\
-    .join(df_taxi_zone_lookup, [df_taxi_trips.DOLocationID == df_taxi_zone_lookup._c0, df_taxi_zone_lookup._c2 == "Battery Park"])\
-    .sort(desc("tip_amount"))\
-    .show(1)
+# df_taxi_trips.filter(month(col("tpep_pickup_datetime")) == 3)\
+#     .join(df_taxi_zone_lookup, [df_taxi_trips.DOLocationID == df_taxi_zone_lookup._c0, df_taxi_zone_lookup._c2 == "Battery Park"])\
+#     .sort(desc("tip_amount"))\
+#     .show(1)
 
 
-end_Q1 = time.time()
-print(f'Q1 time taken: {end_Q1-start_Q1} seconds.')
+# end_Q1 = time.time()
+# print(f'Q1 time taken: {end_Q1-start_Q1} seconds.')
 
 
 
 # Query 2
 
-start_Q2 = time.time()
+# start_Q2 = time.time()
 
 # Να βρεθεί,για κάθε μήνα,η διαδρομή με το υψηλότερο ποσό στα διόδια. Αγνοήστε μηδενικά ποσά
-df_taxi_trips.filter(col("total_amount") > 0)\
-.groupBy(month(col("tpep_pickup_datetime")))\
-.agg(max("Tolls_amount")\
-.alias("max_Tolls_amount"))\
-.sort(asc("month(tpep_pickup_datetime)"))\
-.show()
+# df_taxi_trips.filter(col("total_amount") > 0)\
+# .groupBy(month(col("tpep_pickup_datetime")))\
+# .agg(max("Tolls_amount")\
+# .alias("max_Tolls_amount"))\
+# .sort(asc("month(tpep_pickup_datetime)"))\
+# .show()
 
-end_Q2 = time.time()
+# end_Q2 = time.time()
 
-print(f'Q2 time taken: {end_Q2-start_Q2} seconds.')
+# print(f'Q2 time taken: {end_Q2-start_Q2} seconds.')
+
+# Query 3
+
+#Να βρεθεί, ανά15 ημέρες,ο μέσος όρος της απόστασης και του κόστους για όλες τις διαδρομές με σημείο αναχώρησης διαφορετικό από το σημείο άφιξης.
+# start_Q3_DF = time.time()
+
+# df_taxi_trips.filter(col("PULocationID") != col("DOLocationID"))\
+# .groupBy([dayofmonth(col("tpep_pickup_datetime")),month(col("tpep_pickup_datetime"))])\
+# .agg(sum("trip_distance").alias("sum_trip_distance"),sum("total_amount").alias("sum_total_amount"))\
+# .sort(asc("month(tpep_pickup_datetime)"),asc("dayofmonth(tpep_pickup_datetime)"))\
+# .withColumn("index", row_number().over(Window.orderBy("month(tpep_pickup_datetime)","dayofmonth(tpep_pickup_datetime)")))\
+# .withColumn("group", floor((col("index")-1)/15))\
+# .groupBy("group")\
+# .agg(round(sum("sum_trip_distance")/15,2).alias("avg_trip_distance"),round(sum("sum_total_amount")/15,2).alias("avg_total_amount"))\
+# .show()
+
+# end_Q3_DF = time.time()
+
+# print(f'Q3_DF time taken: {end_Q3_DF-start_Q3_DF} seconds.')
+
+# do the same with rdd
+
+start_Q3_RDD = time.time()
+
+rdd_taxi_trips.count()
+
+end_Q3_RDD = time.time()
+
+print(f'Q3_RDD time taken: {end_Q3_RDD-start_Q3_RDD} seconds.')
+
+
