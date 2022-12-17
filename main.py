a3bdce7e0af8c667f485e19ecae29bd10fb8d37c
NikosVlachakis <nvlachakis1999@gmail.com>
2022-12-17 16:48:12 +0200

    starting rdd q3

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ import sys,time,os
 from pyspark.sql.window import Window
 from pyspark.sql.functions import col, row_number
 
-
-
-
 spark = SparkSession.builder.master("spark://192.168.0.2:7077").getOrCreate()
 print("spark session created")
 
@@ -28,17 +25,17 @@ rdd_taxi_zone_lookup = df_taxi_zone_lookup.rdd
 
 # Query 1
 
-# start_Q1 = time.time()
+start_Q1 = time.time()
 
 # Να βρεθεί η διαδρομή με το μεγαλύτερο φιλοδώρημα (tip)τον Μάρτιο και σημείο άφιξης το "BatteryPark"
-# df_taxi_trips.filter(month(col("tpep_pickup_datetime")) == 3)\
-#     .join(df_taxi_zone_lookup, [df_taxi_trips.DOLocationID == df_taxi_zone_lookup._c0, df_taxi_zone_lookup._c2 == "Battery Park"])\
-#     .sort(desc("tip_amount"))\
-#     .show(1)
+df_taxi_trips.filter(month(col("tpep_pickup_datetime")) == 3)\
+    .join(df_taxi_zone_lookup, [df_taxi_trips.DOLocationID == df_taxi_zone_lookup._c0, df_taxi_zone_lookup._c2 == "Battery Park"])\
+    .sort(desc("tip_amount"))\
+    .show(1)
 
 
-# end_Q1 = time.time()
-# print(f'Q1 time taken: {end_Q1-start_Q1} seconds.')
+end_Q1 = time.time()
+print(f'Q1 time taken: {end_Q1-start_Q1} seconds.')
 
 
 
@@ -46,7 +43,7 @@ rdd_taxi_zone_lookup = df_taxi_zone_lookup.rdd
 
 # start_Q2 = time.time()
 
-# Να βρεθεί,για κάθε μήνα,η διαδρομή με το υψηλότερο ποσό στα διόδια. Αγνοήστε μηδενικά ποσά
+# # Να βρεθεί,για κάθε μήνα,η διαδρομή με το υψηλότερο ποσό στα διόδια. Αγνοήστε μηδενικά ποσά
 # df_taxi_trips.filter(col("total_amount") > 0)\
 # .groupBy(month(col("tpep_pickup_datetime")))\
 # .agg(max("Tolls_amount")\
@@ -77,14 +74,40 @@ rdd_taxi_zone_lookup = df_taxi_zone_lookup.rdd
 
 # print(f'Q3_DF time taken: {end_Q3_DF-start_Q3_DF} seconds.')
 
-# do the same with rdd
+#Να βρεθεί, ανά15 ημέρες,ο μέσος όρος της απόστασης και του κόστους για όλες τις διαδρομές με σημείο αναχώρησης διαφορετικό από το σημείο άφιξης.
+# using RDD
 
-start_Q3_RDD = time.time()
+# start_Q3_RDD = time.time()
 
-rdd_taxi_trips.count()
+# print(rdd_taxi_trips.filter(lambda x: x.PULocationID != x.DOLocationID)\
+# .map(lambda x: (str((x.tpep_pickup_datetime.day,x.tpep_pickup_datetime.month)),(float(x.trip_distance),float(x.total_amount))))\
 
-end_Q3_RDD = time.time()
+# .take(20))
+    
 
-print(f'Q3_RDD time taken: {end_Q3_RDD-start_Q3_RDD} seconds.')
 
 
+
+
+
+
+
+
+# .groupByKey()\
+# .mapValues(list)\
+# .map(lambda x: list(x))\
+# .take(20))
+
+# .mapValues(lambda x: (sum([y.trip_distance for y in x]),sum([y.total_amount for y in x])))\
+# .sortByKey()\
+# .map(lambda x: (x[0][1],x[0][0],x[1][0],x[1][1]))\
+# .map(lambda x: (x[0],x[1],x[2],x[3],(x[0],x[1])))\
+# .map(lambda x: (x[4],(x[2],x[3])))\
+# .groupByKey()\
+# .mapValues(lambda x: (sum([y[0] for y in x])/15,sum([y[1] for y in x])/15))\
+# .sortByKey()\
+# .map(lambda x: (x[0][0],x[0][1],x[1][0],x[1][1]))\
+
+# end_Q3_RDD = time.time()
+
+# print(f'Q3_RDD time taken: {end_Q3_RDD-start_Q3_RDD} seconds.')
