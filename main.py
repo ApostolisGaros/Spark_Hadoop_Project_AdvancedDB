41a01dcd100d729221ba9b66f19e468a85b04e8b
NikosVlachakis <nvlachakis1999@gmail.com>
2022-12-18 00:54:54 +0200

    fixing q2

--- main.py
+++ main.py
@@ -2,13 +2,15 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col, sum, desc, row_number, asc, max, month, dayofmonth, hour,round,floor
 import sys,time,os
 from pyspark.sql.window import Window
-from pyspark.sql.functions import col, row_number
+from pyspark.sql.functions import col
 
 spark = SparkSession.builder.master("spark://192.168.0.2:7077").getOrCreate()
 print("spark session created")
 
 # Path to the data
 hdfs_path = "hdfs://192.168.0.2:9000/user/user/data/"
+
+
 
 # Read the Parquet files from HDFS and create a dataframe
 df_taxi_trips = spark.read.parquet(hdfs_path + "yellow_tripdata_2022-01.parquet", hdfs_path + "yellow_tripdata_2022-02.parquet", hdfs_path + "yellow_tripdata_2022-03.parquet", hdfs_path + "yellow_tripdata_2022-04.parquet", hdfs_path + "yellow_tripdata_2022-05.parquet", hdfs_path + "yellow_tripdata_2022-06.parquet")
@@ -25,17 +27,18 @@ rdd_taxi_zone_lookup = df_taxi_zone_lookup.rdd
 
 # Query 1
 
-start_Q1 = time.time()
+# start_Q1 = time.time()
 
-# Να βρεθεί η διαδρομή με το μεγαλύτερο φιλοδώρημα (tip)τον Μάρτιο και σημείο άφιξης το "BatteryPark"
-df_taxi_trips.filter(month(col("tpep_pickup_datetime")) == 3)\
-    .join(df_taxi_zone_lookup, [df_taxi_trips.DOLocationID == df_taxi_zone_lookup._c0, df_taxi_zone_lookup._c2 == "Battery Park"])\
-    .sort(desc("tip_amount"))\
-    .show(1)
+# # Να βρεθεί η διαδρομή με το μεγαλύτερο φιλοδώρημα (tip)τον Μάρτιο και σημείο άφιξης το "BatteryPark"
+# df_taxi_trips.filter(month(col("tpep_pickup_datetime")) == 3)\
+#     .join(df_taxi_zone_lookup, [df_taxi_trips.DOLocationID == df_taxi_zone_lookup._c0, df_taxi_zone_lookup._c2 == "Battery Park"])\
+#     .sort(desc("tip_amount"))\
+#     .drop("_c0","_c1","_c2","_c3")\
+#     .show(1)
 
 
-end_Q1 = time.time()
-print(f'Q1 time taken: {end_Q1-start_Q1} seconds.')
+# end_Q1 = time.time()
+# print(f'Q1 time taken: {end_Q1-start_Q1} seconds.')
 
 
 
@@ -44,11 +47,13 @@ print(f'Q1 time taken: {end_Q1-start_Q1} seconds.')
 # start_Q2 = time.time()
 
 # # Να βρεθεί,για κάθε μήνα,η διαδρομή με το υψηλότερο ποσό στα διόδια. Αγνοήστε μηδενικά ποσά
-# df_taxi_trips.filter(col("total_amount") > 0)\
+# df_taxi_trips.filter(col("Tolls_amount") > 0)\
 # .groupBy(month(col("tpep_pickup_datetime")))\
 # .agg(max("Tolls_amount")\
 # .alias("max_Tolls_amount"))\
 # .sort(asc("month(tpep_pickup_datetime)"))\
+# .join(df_taxi_trips, [month(col("tpep_pickup_datetime")) == col("month(tpep_pickup_datetime)"), col("Tolls_amount") == col("max_Tolls_amount")])\
+# .drop("month(tpep_pickup_datetime)","max_Tolls_amount")\
 # .show()
 
 # end_Q2 = time.time()
