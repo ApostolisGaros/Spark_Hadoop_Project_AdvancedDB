0e139b4fcedd0d620474527746e0675b0a218301
NikosVlachakis <nvlachakis1999@gmail.com>
2022-12-20 13:50:35 +0200

    finish rdd

--- main.py
+++ main.py
@@ -95,6 +95,7 @@ print(rdd_taxi_trips.filter(lambda x: x.PULocationID != x.DOLocationID)\
 .map(lambda x: (int(str(x[1]/15)[0]),(x[0][1][0],x[0][1][1],1)))\
 .reduceByKey(lambda x,y: (x[0]+y[0],x[1]+y[1],x[2]+y[2]))\
 .map(lambda x: (x[0],(x[1][0]/x[1][2],x[1][1]/x[1][2])))\
+.sortByKey()\
 .take(20))
 
 
